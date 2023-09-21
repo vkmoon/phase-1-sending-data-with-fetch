e67cda29605d081017f7999fe8cd7f63d8382068
Valeria <valeriakirkbride@gmail.com>
2023-09-20 19:38:03 -0600

    send data fetch

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(name, email) {
+    const fetchReturn =
+    fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email,
+    }),
+})
+    .then(res => res.json())
+    .then(object => document.body.innerHTML = object.id)
+    .catch(error => document.body.innerHTML = error)
+return fetchReturn;
+}
